3869310dd2be297535ab78d9d8cc7c2f3ce8177b
Atio716 <37644204+Atio716@users.noreply.github.com>
2018-05-18 09:31:46 +0900

    Update Signup

--- lib/Controller/Signup.php
+++ lib/Controller/Signup.php
@@ -14,7 +14,31 @@ class Signup extends \MyApp\Controller {
       $this->postProcess();
     }
   }
+protected function postProcess() {
+    // validate
+    try {
+      $this->_validate();
+    } catch (\MyApp\Exception\InvalidEmail $e) {
 
+    } catch (\MyApp\Exception\InvalidPassword $e) {
+
+    }
+
+    // create user
+
+    // redirect to login
+
+  }
+
+  private function _validate() {
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      throw new \MyApp\Exception\InvalidEmail();
+    }
+
+    if (!preg_match('/\A[a-zA-Z0-9]+\z/', $_POST['passwor'])) {
+      throw new \MyApp\Exception\InvalidPassword();
+    }
+  }
 
 
 }
